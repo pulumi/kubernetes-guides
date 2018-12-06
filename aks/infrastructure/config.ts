f9d7e8b2fdcc87ca190a0d7713d994e8c4c7a0d9
Levi Blackstone <levi@pulumi.com>
2018-12-06 15:27:32 -0700

    Add AKS identity and infrastructure (#2)

--- aks/infrastructure/config.ts
+++ aks/infrastructure/config.ts
@@ -18,27 +18,19 @@ const config = new pulumi.Config();
 
 export const identityStackName = config.require("identityStackName");
 const identityStack = new pulumi.StackReference(identityStackName);
-
 //
-// GCP-specific config.
-//
-
-// project is the GCP project you are going to deploy to.
-export const project = identityStack.getOutput("project");
-
-// zone is the zone in which to build the cluster.
-export const zone = new pulumi.Config("gcp").get("zone");
-
-//
-// Kubernetes-specific config.
+// Azure-specific config.
 //
 
-// envName is the name of the environment represented by this cluster.
-export const envName = config.require("envName");
+export const resourceGroupName = identityStack.getOutput("resourceGroupName");
+export const location = identityStack.getOutput("location");
+export const applicationID = identityStack.getOutput("applicationID");
+export const servicePrincipalPassword = identityStack.getOutput("servicePrincipalPassword");
 
-// nodeCount is the number of cluster nodes to provision. Defaults to 5 if unspecified.
-export const nodeCount = config.getNumber("nodeCount");
+//
+// AKS-specific config.
+//
 
-// nodeMachineType is the machine type to use for cluster nodes. Defaults to n1-standard-1 if unspecified.
-// See https://cloud.google.com/compute/docs/machine-types for more details on available machine types.
-export const nodeMachineType = config.get("nodeMachineType");
+export const nodeCount = config.getNumber("nodeCount") || 2;
+export const nodeSize = config.get("nodeSize") || "Standard_D2_v2";
+export const sshPublicKey = config.require("sshPublicKey");
