c3e4ab240e9fd837bd5a58a57971ec23f9f17e16
Mike Metral <1112768+metral@users.noreply.github.com>
2019-10-08 12:09:40 -0700

    Reorg directory tree by cloud provider and stack

--- gcp/identity/scripts/generate-credentials.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -o nounset -o errexit -o pipefail
-
-pulumi stack output infraCiClientSecret > infra-ci-client-secret.json
-pulumi stack output k8sAppDevCiClientSecret > k8s-app-dev-ci-client-secret.json
