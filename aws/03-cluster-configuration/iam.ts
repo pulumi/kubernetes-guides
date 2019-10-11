db88c4ae4a68e5616824ab231f879f4a0f5e74d1
Mike Metral <1112768+metral@users.noreply.github.com>
2019-10-11 00:13:07 -0700

    aws/k8s-cluster: init add of cluster configuration

--- aws/03-cluster-configuration/iam.ts
+++ aws/03-cluster-configuration/iam.ts
@@ -1,41 +1,4 @@
 import * as aws from "@pulumi/aws";
-import * as pulumi from "@pulumi/pulumi";
-
-const managedPolicyArns: string[] = [
-    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
-    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-];
-
-// Creates a role and attaches the EKS worker node IAM managed policies
-export function createRole(name: string): aws.iam.Role {
-    const role = new aws.iam.Role(name, {
-        assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({
-            Service: "ec2.amazonaws.com",
-        }),
-    });
-
-    let counter = 0;
-    for (const policy of managedPolicyArns) {
-        // Create RolePolicyAttachment without returning it.
-        const rpa = new aws.iam.RolePolicyAttachment(`${name}-policy-${counter++}`,
-            { policyArn: policy, role: role },
-        );
-    }
-
-    return role;
-}
-
-// Creates a collection of IAM roles.
-export function createRoles(name: string, quantity: number): aws.iam.Role[] {
-    const roles: aws.iam.Role[] = [];
-
-    for (let i = 0; i < quantity; i++) {
-        roles.push(createRole(`${name}-role-${i}`));
-    }
-
-    return roles;
-}
 
 // Creates a collection of IAM instance profiles from the given roles.
 export function createInstanceProfiles(name: string, roles: aws.iam.Role[]): aws.iam.InstanceProfile[] {
--- aws/03-cluster-configuration/index.ts
+++ aws/03-cluster-configuration/index.ts
@@ -1,53 +1,183 @@
+import * as aws from "@pulumi/aws";
+import * as eks from "@pulumi/eks";
 import * as k8s from "@pulumi/kubernetes";
 import * as pulumi from "@pulumi/pulumi";
-import * as iam from "./iam";
-import * as utils from "./utils";
+import { config } from "./config";
 
 const projectName = pulumi.getProject();
 
-// Export VPC ID and Subnets.
-export const vpcId = vpc.id;
-export const allVpcSubnets = vpc.privateSubnetIds.concat(vpc.publicSubnetIds);
-
-// Create 3 IAM Roles and matching InstanceProfiles to use with the nodegroups.
-const roles = iam.createRoles(projectName, 3);
-const instanceProfiles = iam.createInstanceProfiles(projectName, roles);
+export const adminsIamRoleArn = config.adminsIamRoleArn
+export const devsIamRoleArn = config.devsIamRoleArn
+export const stdNodegroupIamRoleArn = config.stdNodegroupIamRoleArn
+export const perfNodegroupIamRoleArn = config.perfNodegroupIamRoleArn
+const adminsIamRoleName = adminsIamRoleArn.apply(s => s.split("/")).apply(s => s[1])
+const devsIamRoleName = devsIamRoleArn.apply(s => s.split("/")).apply(s => s[1])
+const stdNodegroupIamRoleName = stdNodegroupIamRoleArn.apply(s => s.split("/")).apply(s => s[1])
+const perfNodegroupIamRoleName = perfNodegroupIamRoleArn.apply(s => s.split("/")).apply(s => s[1])
 
 // Create an EKS cluster.
-const myCluster = new eks.Cluster(`${projectName}`, {
-    version: "1.14",
-    vpcId: vpcId,
-    subnetIds: allVpcSubnets,
+const cluster = new eks.Cluster(`${projectName}`, {
+    instanceRoles: [
+        aws.iam.Role.get("adminsIamRole", stdNodegroupIamRoleName),
+        aws.iam.Role.get("devsIamRole", perfNodegroupIamRoleName),
+    ],
+    roleMappings: [
+        {
+            roleArn: config.adminsIamRoleArn,
+            groups: ["system:masters"],
+            username: "pulumi:admins",
+        },
+        {
+            roleArn: config.devsIamRoleArn,
+            groups: ["pulumi:devs"],
+            username: "pulumi:alice",
+        },
+    ],
+    vpcId: config.vpcId,
+    publicSubnetIds: config.publicSubnetIds,
+    privateSubnetIds: config.privateSubnetIds,
+    storageClasses: {
+        "gp2-encrypted": { type: "gp2", encrypted: true},
+        "sc1": { type: "sc1"}
+    },
     nodeAssociatePublicIpAddress: false,
     skipDefaultNodeGroup: true,
     deployDashboard: false,
-    instanceRoles: roles,
-    enabledClusterLogTypes: ["api", "audit", "authenticator",
-        "controllerManager", "scheduler"],
+    version: "1.14",
+    tags: {
+        "Project": "k8s-aws-cluster",
+        "Org": "pulumi",
+    },
+    clusterSecurityGroupTags: { "ClusterSecurityGroupTag": "true" },
+    nodeSecurityGroupTags: { "NodeSecurityGroupTag": "true" },
+    enabledClusterLogTypes: ["api", "audit", "authenticator", "controllerManager", "scheduler"],
+    // endpointPublicAccess: false,     // Requires bastion to access cluster API endpoint
+    // endpointPrivateAccess: true,     // Requires bastion to access cluster API endpoint
 });
-export const kubeconfig = myCluster.kubeconfig;
-export const clusterName = myCluster.core.cluster.name;
+
+// Export the cluster details.
+export const kubeconfig = cluster.kubeconfig;
+export const clusterName = cluster.core.cluster.name;
 
 // Create a Standard node group of t2.medium workers.
-const ngStandard = utils.createNodeGroup(`${projectName}-ng-standard`, {
-    ami: "ami-03a55127c613349a7", // k8s v1.13.7 in us-west-2
+const ngStandard = new eks.NodeGroup(`${projectName}-ng-standard`, {
+    cluster: cluster,
+    instanceProfile: new aws.iam.InstanceProfile("ng-standard", {role: stdNodegroupIamRoleName}),
+    nodeAssociatePublicIpAddress: false,
+    nodeSecurityGroup: cluster.nodeSecurityGroup,
+    clusterIngressRule: cluster.eksClusterIngressRule,
+    amiId: "ami-0ca5998dc2c88e64b", // k8s v1.14.7 in us-west-2
     instanceType: "t2.medium",
     desiredCapacity: 3,
-    cluster: myCluster,
-    instanceProfile: instanceProfiles[0],
+    minSize: 3,
+    maxSize: 10,
+    labels: {"amiId": "ami-0ca5998dc2c88e64b"},
+    cloudFormationTags: clusterName.apply(clusterName => ({
+        "CloudFormationGroupTag": "true",
+        "k8s.io/cluster-autoscaler/enabled": "true",
+        [`k8s.io/cluster-autoscaler/${clusterName}`]: "true",
+    })),
+}, {
+    providers: { kubernetes: cluster.provider},
 });
 
-// Create a 2xlarge node group of t3.2xlarge workers with taints on the nodes
-// dedicated for the NGINX Ingress Controller.
-const ng2xlarge = utils.createNodeGroup(`${projectName}-ng-2xlarge`, {
-    ami: "ami-0355c210cb3f58aa2", // k8s v1.12.7 in us-west-2
+// Create a 2xlarge node group of t3.2xlarge workers with taints for special workloads.
+const ng2xlarge = new eks.NodeGroup(`${projectName}-ng-2xlarge`, {
+    cluster: cluster,
+    instanceProfile: new aws.iam.InstanceProfile("ng-2xlarge", {role: perfNodegroupIamRoleName}),
+    nodeAssociatePublicIpAddress: false,
+    nodeSecurityGroup: cluster.nodeSecurityGroup,
+    clusterIngressRule: cluster.eksClusterIngressRule,
+    amiId: "ami-0ca5998dc2c88e64b", // k8s v1.14.7 in us-west-2
     instanceType: "t3.2xlarge",
-    desiredCapacity: 3,
-    cluster: myCluster,
-    instanceProfile: instanceProfiles[1],
-    taints: {"nginx": { value: "true", effect: "NoSchedule"}},
+    desiredCapacity: 5,
+    minSize: 5,
+    maxSize: 10,
+    labels: {"amiId": "ami-0ca5998dc2c88e64b"},
+    taints: { "special": { value: "true", effect: "NoSchedule"}},
+    cloudFormationTags: clusterName.apply(clusterName => ({
+        "CloudFormationGroupTag": "true",
+        "k8s.io/cluster-autoscaler/enabled": "true",
+        [`k8s.io/cluster-autoscaler/${clusterName}`]: "true",
+    })),
+}, {
+    providers: { kubernetes: cluster.provider},
 });
 
-// Create a Kubernetes namespace for apps.
-const namespace = new k8s.core.v1.Namespace("apps", undefined, { provider: myCluster.provider });
-export const namespaceName = namespace.metadata.name;
+// Create Kubernetes namespaces.
+const clusterSvcsNamespace = new k8s.core.v1.Namespace("cluster-svcs", undefined, { provider: cluster.provider });
+export const clusterSvcsNamespaceName = clusterSvcsNamespace.metadata.name;
+
+const appSvcsNamespace = new k8s.core.v1.Namespace("app-svcs", undefined, { provider: cluster.provider });
+export const appSvcsNamespaceName = appSvcsNamespace.metadata.name;
+
+const appNamespace = new k8s.core.v1.Namespace("apps", undefined, { provider: cluster.provider });
+export const appNamespaceName = appNamespace.metadata.name;
+
+// Create a resource quota in the apps namespace.
+const quotaAppNamespace = new k8s.core.v1.ResourceQuota("apps", {
+    metadata: {namespace: appNamespaceName},
+    spec: {
+        hard: {
+            cpu: "20",
+            memory: "1Gi",
+            pods: "10",
+            replicationcontrollers: "20",
+            resourcequotas: "1",
+            services: "5",
+        },
+    }
+},{
+    provider: cluster.provider
+});
+
+// Create a limited role for the `pulumi:devs` to use in the apps namespace.
+let devsGroupRole = new k8s.rbac.v1.Role("pulumi-devs",
+    {
+        metadata: {namespace: appNamespaceName},
+        rules: [
+            {
+                apiGroups: ["", "apps"],
+                resources: ["pods", "services", "deployments", "replicasets", "persistentvolumeclaims"],
+                verbs: ["get", "list", "watch", "create", "update", "delete"],
+            },
+        ],
+    },
+    {provider: cluster.provider},
+);
+
+// Bind the `pulumi:devs` RBAC group to the new, limited role.
+let devsGroupRoleBinding = new k8s.rbac.v1.RoleBinding("pulumi-devs", {
+    metadata: {namespace: appNamespaceName},
+    subjects: [{
+        kind: "Group",
+        name: "pulumi:devs",
+    }],
+    roleRef: {
+        kind: "Role",
+        name: devsGroupRole.metadata.name,
+        apiGroup: "rbac.authorization.k8s.io",
+    },
+}, {provider: cluster.provider});
+
+
+// Create a persistent volume using a persistent volume claim.
+if (cluster.core.storageClasses) {
+    if ("gp2-encrypted" in cluster.core.storageClasses) {
+        cluster.core.storageClasses["gp2-encrypted"].apply(sc => {
+            sc.metadata.name.apply(name => {
+                if (name) {
+                    const myPvc = new k8s.core.v1.PersistentVolumeClaim("mypvc", {
+                        spec: {
+                            accessModes: ["ReadWriteOnce"],
+                            storageClassName: name,
+                            resources: {requests: {storage: "1Gi"}}
+                        }
+                    },
+                        { provider: cluster.provider }
+                    );
+                }
+            });
+        });
+    }
+}
