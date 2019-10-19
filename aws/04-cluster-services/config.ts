f86dbf1e21880286d32146a258caf405130940ab
Mike Metral <1112768+metral@users.noreply.github.com>
2019-10-18 18:41:01 -0700

    Add aws cluster services: fluentd-cloudwatch worker logging

---
+++ aws/04-cluster-services/config.ts
@@ -0,0 +1,22 @@
+import * as pulumi from "@pulumi/pulumi";
+
+let pulumiConfig = new pulumi.Config();
+
+// Existing Pulumi stack reference in the format:
+// <organization>/<project>/<stack> e.g. "myUser/myProject/dev"
+const identityStackRef = new pulumi.StackReference(pulumiConfig.require("identityStackRef"));
+const clusterStackRef = new pulumi.StackReference(pulumiConfig.require("clusterStackRef"));
+
+export const config = {
+    // Identity
+    stdNodegroupIamRoleArn : identityStackRef.getOutput("stdNodegroupIamRoleArn"),
+    perfNodegroupIamRoleArn: identityStackRef.getOutput("perfNodegroupIamRoleArn"),
+
+    // Cluster
+    kubeconfig: clusterStackRef.getOutput("kubeconfig"),
+    clusterName: clusterStackRef.getOutput("clusterName"),
+    clusterSvcsNamespaceName: clusterStackRef.getOutput("clusterSvcsNamespaceName"),
+    appsSvcsNamespaceName: clusterStackRef.getOutput("appSvcsNamespaceName"),
+    appsNamespaceName: clusterStackRef.getOutput("appsNamespaceName"),
+};
+
