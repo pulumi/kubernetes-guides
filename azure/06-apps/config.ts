c3f36e201a5183bb78ad611f850ee2445040e58a
Mikhail Shilkov <shilkov@gmail.com>
2019-10-24 17:20:36 +0200

    Apps stack

---
+++ azure/06-apps/config.ts
@@ -0,0 +1,29 @@
+// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+
+import * as pulumi from "@pulumi/pulumi";
+
+const pulumiConfig = new pulumi.Config();
+
+// Existing Pulumi stack reference in the format:
+// <organization>/<project>/<stack> e.g. "myUser/myProject/dev"
+
+const identityStackRef = new pulumi.StackReference(pulumiConfig.require("identityStackRef"));
+const infraStackRef = new pulumi.StackReference(pulumiConfig.require("infraStackRef"));
+const clusterStackRef = new pulumi.StackReference(pulumiConfig.require("clusterStackRef"));
+
+export const config = {
+    // Resource Group
+    resourceGroupName:        identityStackRef.getOutput("resourceGroupName"),
+
+    // Identity
+    adApplicationId:          identityStackRef.getOutput("adApplicationId"),
+    adSpPassword:             identityStackRef.getOutput("adSpPassword"),
+
+    // Infrastructure / Networking
+    subnetId:                 infraStackRef.getOutput("subnetId"),
+    logAnalyticsWorkspaceId:  infraStackRef.getOutput("logAnalyticsWorkspaceId"),
+
+    // AKS Cluster
+    clusterId:                clusterStackRef.getOutput("clusterId"),
+    kubeconfig:               clusterStackRef.getOutput("kubeconfig"),
+};
