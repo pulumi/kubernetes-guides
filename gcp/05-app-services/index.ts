cc5daa3d12f4abc3789ce04e40e6cfac49d2c9e0
Alex Clemmer <clemmer.alexander@gmail.com>
2019-10-28 00:37:53 -0700

    Initial implementation of identity and infra stacks

---
+++ gcp/05-app-services/index.ts
@@ -0,0 +1,67 @@
+import * as gcp from "@pulumi/gcp";
+import * as k8s from "@pulumi/kubernetes";
+import * as pulumi from "@pulumi/pulumi";
+import * as random from "@pulumi/random";
+import { config } from "./config";
+
+const projectName = pulumi.getProject();
+
+// const privateSubnetIds = config.privateSubnetIds;
+// const securityGroupIds = config.securityGroupIds;
+const clusterName = config.clusterName;
+
+// Generate a strong password for the Postgres DB.
+const postgresDbPassword = new random.RandomString(
+    `${projectName}-db-password`,
+    {
+        length: 20,
+        special: true,
+    },
+    { additionalSecretOutputs: ["result"] },
+).result;
+
+const db = new gcp.sql.DatabaseInstance("postgresdb", {
+    project: config.project,
+    region: gcp.config.zone,
+    databaseVersion: "POSTGRES_9_6",
+    settings: { tier: "db-f1-micro" },
+});
+const user = new gcp.sql.User("default", {
+    project: config.project,
+    instance: db.name,
+    password: postgresDbPassword,
+});
+
+// Create a Secret from the DB connection information.
+const provider = new k8s.Provider("eks-provider", {
+    kubeconfig: config.kubeconfig,
+});
+const dbConn = new k8s.core.v1.Secret(
+    "postgres-db-conn",
+    {
+        data: {
+            host: db.privateIpAddress.apply(addr => Buffer.from(addr).toString("base64")),
+            port: Buffer.from("5432").toString("base64"),
+            username: user.name.apply(user => Buffer.from(user).toString("base64")),
+            password: postgresDbPassword.apply(pass => Buffer.from(pass).toString("base64")),
+        },
+    },
+    { provider: provider },
+);
+
+const cache = new gcp.redis.Instance("redis", {
+    tier: "STANDARD_HA",
+    memorySizeGb: 1,
+    redisVersion: "REDIS_3_2",
+});
+
+// Create a ConfigMap from the cache connection information.
+const cacheConn = new k8s.core.v1.ConfigMap(
+    "postgres-db-conn",
+    {
+        data: {
+            host: cache.host.apply(addr => Buffer.from(addr).toString("base64")),
+        },
+    },
+    { provider: provider },
+);
