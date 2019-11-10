80c6196967f2ba1abfc59b5ed0ccd571a643c6fe
Mike Metral <1112768+metral@users.noreply.github.com>
2019-11-09 19:02:40 -0800

    app-svcs: cleanup, comments, minor nits

--- gcp/05-app-services/index.ts
+++ gcp/05-app-services/index.ts
@@ -20,22 +20,27 @@ const postgresDbPassword = new random.RandomString(
     { additionalSecretOutputs: ["result"] },
 ).result;
 
+// Create a Postgres DB instance.
 const db = new gcp.sql.DatabaseInstance("postgresdb", {
     project: config.project,
-    region: gcp.config.zone,
+    region: "us-west1",
     databaseVersion: "POSTGRES_9_6",
     settings: { tier: "db-f1-micro" },
 });
+
+// Configure a new SQL user.
 const user = new gcp.sql.User("default", {
     project: config.project,
     instance: db.name,
     password: postgresDbPassword,
 });
 
-// Create a Secret from the DB connection information.
-const provider = new k8s.Provider("eks-provider", {
+// Create a new k8s provider.
+const provider = new k8s.Provider("provider", {
     kubeconfig: config.kubeconfig,
 });
+
+// Create a Secret from the DB connection information.
 const dbConn = new k8s.core.v1.Secret(
     "postgres-db-conn",
     {
@@ -49,6 +54,7 @@ const dbConn = new k8s.core.v1.Secret(
     { provider: provider },
 );
 
+// Create a Redis instance.
 const cache = new gcp.redis.Instance("redis", {
     tier: "STANDARD_HA",
     memorySizeGb: 1,
