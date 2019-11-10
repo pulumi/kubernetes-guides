37004b382bdd7f741b8eeaaf810979ee528118ce
Levi Blackstone <levi@pulumi.com>
2019-11-09 20:18:29 -0700

    Add job example

---
+++ apps/job/config.ts
@@ -0,0 +1,22 @@
+import * as pulumi from "@pulumi/pulumi";
+
+let pulumiConfig = new pulumi.Config();
+
+// Existing Pulumi stack reference in the format:
+// <organization>/<project>/<stack> e.g. "myUser/myProject/dev"
+const infraStackRef = new pulumi.StackReference(pulumiConfig.require("infraStackRef"));
+const clusterStackRef = new pulumi.StackReference(pulumiConfig.require("clusterStackRef"));
+
+export const config = {
+    // Infra
+    privateSubnetIds: infraStackRef.getOutput("privateSubnetIds"),
+    publicSubnetIds: infraStackRef.getOutput("publicSubnetIds"),
+
+    // Cluster
+    kubeconfig: clusterStackRef.getOutput("kubeconfig"),
+    clusterName: clusterStackRef.getOutput("clusterName"),
+    securityGroupIds: clusterStackRef.getOutput("securityGroupIds"),
+    clusterSvcsNamespaceName: clusterStackRef.getOutput("clusterSvcsNamespaceName"),
+    appSvcsNamespaceName: clusterStackRef.getOutput("appSvcsNamespaceName"),
+    appsNamespaceName: clusterStackRef.getOutput("appsNamespaceName"),
+};
