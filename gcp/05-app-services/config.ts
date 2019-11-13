5dbc27ebcbbe2f34f72d32dc65b801b87dc2681c
Mike Metral <1112768+metral@users.noreply.github.com>
2019-11-12 16:56:35 -0800

    app-svcs/gcp: add README

--- gcp/05-app-services/config.ts
+++ gcp/05-app-services/config.ts
@@ -5,7 +5,6 @@ let pulumiConfig = new pulumi.Config();
 // Existing Pulumi stack reference in the format:
 // <organization>/<project>/<stack> e.g. "myUser/myProject/dev"
 const identityStackRef = new pulumi.StackReference(pulumiConfig.require("identityStackRef"));
-const infraStackRef = new pulumi.StackReference(pulumiConfig.require("infraStackRef"));
 const clusterStackRef = new pulumi.StackReference(pulumiConfig.require("clusterStackRef"));
 
 export const config = {
